0cc4f7468e7b59b69b3f234028f8f5f15c9b46a4
anna.karutina <anna.karutina@khk.ee>
2017-03-15 15:32:36 +0200

    Koostatud rakenduse konfiguratsiooni esmaversioon

---
+++ conf.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anna.karutina
+ * Date: 15.03.2017
+ * Time: 15:24
+ */
+// defineerime vajalikud konstandid
+define('CLASSES_DIR', 'classes/'); // classes kataloogi nime konstant
+define('TMPL_DIR', 'tmpl/'); // tmpl kataloogi nime konstant
+?>
