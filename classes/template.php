0c58edf13a3d5a766f7d48f098b61a80a9a1caf7
anna.karutina <anna.karutina@khk.ee>
2017-03-22 12:20:59 +0200

    Koostatud template klassi esmaversioon

---
+++ classes/template.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: anna.karutina
+ * Date: 22.03.2017
+ * Time: 12:11
+ */
+class template
+{// klassi algus
+	// template klassi omadused - muutujad
+	var $file = ''; // html malli faili nimi
+	var $content = false; // html malli faili sisu
+	var $vars = array(); // html vaade sisu - reaalsed väärtused
+}// klassi lõpp
+?>
