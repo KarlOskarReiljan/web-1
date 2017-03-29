9081c8f837c7b47129b81f884cabd50e46213d4a
anna.karutina <anna.karutina@khk.ee>
2017-03-29 15:04:05 +0300

    Lisatud andmepaari lisamine lingile

--- classes/linkobject.php
+++ classes/linkobject.php
@@ -24,10 +24,11 @@ class linkobject extends http
 	
 	// andmete paari koostamine kujul
 	// nimi=väärtus&nimi1=väärtus1 jne
-	function addToLink($link, $nimi, $val){
+	function addToLink(&$link, $name, $val){
 		if($link != ''){
 			$link = $link.$this->delim;
 		}
-		$link = $link.
+		$link = $link.fixUrl($name).$this->eq.fixUrl($val);
+		echo $link;
 	}// addToLink
 }// klassi lõpp
