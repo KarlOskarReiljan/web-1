9c0da38a755072aab4beef82e19eb6e737ddfea1
anna.karutina <anna.karutina@khk.ee>
2017-03-29 13:52:53 +0300

    Koostatud http klassi algversioon

---
+++ classes/http.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: anna.karutina
+ * Date: 29.03.2017
+ * Time: 13:45
+ */
+class http
+{// klassi algus
+	// klassi muutujad
+	var $vars = array(); // http päringute andmed
+	var $server = array(); // serveri (masina) andmed
+	// klassi meetodid
+	// paneme algandmed paika - initsialiseerime neid
+	function init(){
+		$this->vars = array_merge($_GET, $_POST, $_FILES);
+		$this->server = $_SERVER;
+	}// init
+}// klassi lõpp
+?>
