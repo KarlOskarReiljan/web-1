0cf82ec1552c205b8f1d73f2ad506847905170f9
anna.karutina <anna.karutina@khk.ee>
2017-04-05 09:12:40 +0300

    Koostatud tegevushalduse esmaversioon

---
+++ act.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anna.karutina
+ * Date: 5.04.2017
+ * Time: 9:03
+ */
+$act = $http->get('act'); // küsime hetkel valitud tegevus
+// koostame otsitava faili nimi - failisüsteemi jaoks
+$fn = ACTS_DIR.str_replace('.', '/', $act).'.php';
+// kui selline fail olemas ja lugemiseks lubatud
+if(file_exists($fn) and is_file($fn) and is_readable($fn)){
+	// loeme sisu
+	require_once $fn;
+} else {
+	echo 'sobiva faili pole';
+}
